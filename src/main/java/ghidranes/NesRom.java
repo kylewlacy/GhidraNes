7c589834c5895bae267721323865f3e40d0c12ba
Kyle Lacy <kylelacy+git@pm.me>
2019-04-07 19:01:48 -0700

    Add `NesRom` class to load sections from an iNES ROM

---
+++ src/main/java/ghidranes/NesRom.java
@@ -0,0 +1,38 @@
+package ghidranes;
+
+import java.io.IOException;
+import java.io.InputStream;
+import ghidranes.errors.NesRomEofException;
+
+public class NesRom {
+	NesRomHeader header;
+	byte[] trainerBytes;
+	byte[] prgRom;
+	byte[] chrRom;
+
+	public NesRom(NesRomHeader romHeader, InputStream bytes) throws NesRomEofException, IOException {
+		if (romHeader.hasTrainer) {
+			trainerBytes = bytes.readNBytes(512);
+			if (trainerBytes.length < 512) {
+				throw new NesRomEofException();
+			}
+		}
+		else {
+			trainerBytes = new byte[0];
+		}
+
+		byte[] prgRomBytes = bytes.readNBytes(romHeader.prgRomSizeBytes);
+		if (prgRomBytes.length < romHeader.prgRomSizeBytes) {
+			throw new NesRomEofException();
+		}
+
+		byte[] chrRomBytes = bytes.readNBytes(romHeader.chrRomSizeBytes);
+		if (chrRomBytes.length < romHeader.chrRomSizeBytes) {
+			throw new NesRomEofException();
+		}
+
+		this.header = romHeader;
+		this.prgRom = prgRomBytes;
+		this.chrRom = chrRomBytes;
+	}
+}
