730ef2fcf6f962dc1fffa5a494ae5e26f8589eba
Kyle Lacy <kylelacy+git@pm.me>
2019-10-16 23:28:02 -0700

    [WIP] Fix iNES mapper mapping for UXROM

--- src/main/java/ghidranes/mappers/NesMapper.java
+++ src/main/java/ghidranes/mappers/NesMapper.java
@@ -17,7 +17,7 @@ public abstract class NesMapper {
 		switch (mapperNum) {
 		case 0:
 			return new NromMapper();
-		case 2:
+		case 1:
 			return new UxromMapper();
 		default:
 			throw new UnimplementedNesMapperException(mapperNum);
