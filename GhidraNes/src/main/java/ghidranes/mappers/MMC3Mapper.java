3881d4d51c61b079666ed7f4ea55b61d5982ba72
Grason Humphrey <gtiger43@gmail.com>
2025-03-20 09:11:15 -0400

    Fix MMC3 optionally swappable ROM bank mapping

--- GhidraNes/src/main/java/ghidranes/mappers/MMC3Mapper.java
+++ GhidraNes/src/main/java/ghidranes/mappers/MMC3Mapper.java
@@ -39,7 +39,9 @@ public class MMC3Mapper extends NesMapper {
 			//CPU $A000-$BFFF: 8 KB switchable PRG ROM bank
 			MemoryBlockDescription.initialized(0x8000, 0x2000, "PRG" + bank, romPermissions, rombankBytes, bank != 0, monitor)
 				.create(program);
-			MemoryBlockDescription.initialized(0xA000, 0x2000, "PRG" + bank + "_MIRROR", romPermissions, rombankBytes, bank != 0, monitor)
+			MemoryBlockDescription.initialized(0xA000, 0x2000, "PRG" + bank + "_MIRROR1", romPermissions, rombankBytes, bank != 0, monitor)
+				.create(program);
+			MemoryBlockDescription.initialized(0xC000, 0x2000, "PRG" + bank + "_MIRROR2", romPermissions, rombankBytes, bank != 0, monitor)
 				.create(program);
 		}
 		
@@ -47,7 +49,9 @@ public class MMC3Mapper extends NesMapper {
 		int romPermissions = MemoryBlockDescription.READ | MemoryBlockDescription.EXECUTE;
 		byte[] rombankBytes = Arrays.copyOfRange(rom.prgRom, bank*0x2000, (bank+1)*0x2000);
 		//CPU $C000-$DFFF (or $8000-$9FFF): 8 KB PRG ROM bank, fixed to the second-last bank
-		MemoryBlockDescription.initialized(0xC000, 0x2000, "PRG" + bank + "_FIXED1", romPermissions, rombankBytes, false, monitor)
+		MemoryBlockDescription.initialized(0x8000, 0x2000, "PRG" + bank + "_FIXED1", romPermissions, rombankBytes, true, monitor)
+			.create(program);
+		MemoryBlockDescription.initialized(0xC000, 0x2000, "PRG" + bank + "_FIXED1_MIRROR", romPermissions, rombankBytes, true, monitor)
 			.create(program);
 		
 		bank += 1;
